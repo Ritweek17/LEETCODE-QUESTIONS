be6de111e6484e101c6eed0328e91a41418ecbdd
Ritweek Dubey <ritweekdubey2006@gmail.com>
2026-06-30 23:54:25 +0530

    Time: 0 ms (100%), Space: 28.2 MB (30.34%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int element = 0;
+        int count = 0;
+
+        for (int x : nums) {
+            if (count == 0) {
+                element = x;
+            }
+
+            if (x == element) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return element;
+    }
+};
